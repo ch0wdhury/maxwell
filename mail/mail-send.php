ef6fdb1fb2fd4dee9ea14a0f71550750dbaf37a1
Ashiq Ali Chowdhury <43188066+ch0wdhury@users.noreply.github.com>
2019-04-03 18:49:35 +0600

    Add files via upload

--- mail/mail-send.php
+++ mail/mail-send.php
@@ -1,11 +1,11 @@
 <?php
-if (isset($_POST['name']) && isset($_POST['email'])) {
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $to = 'ashiq.ali.chowdhury@gmail.com';
     $subject = "New Message on KristjanMark.com";
-    $message = = $_POST['message'];
+    $message = $_POST['message'];
 
 //headers
 $headers = "From: ".$name." <".$email.">\r\n";
@@ -14,7 +14,7 @@ $headers = "MIME-Version: 1.0\r\n";
 $headers = "Content-type: text/html; charset-utf-8";
 
 //send
-$send = mail($to, $subject, $message, $headers);
+$send = mail($to, $subject, $phone, $message, $headers);
 if ($send) {
     echo '<br>';
     echo "Success. Thanks for Your Message.";
